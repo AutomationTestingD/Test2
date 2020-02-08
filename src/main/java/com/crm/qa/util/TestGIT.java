34c6f4e40987ec1fa271ca507ae88f5be46152dd
AutomationTestingD <sdk.wgl@gmaill.com>
2020-02-08 10:22:48 -0800

    updated TestGit file with sysout for pull from idea

--- src/main/java/com/crm/qa/util/TestGIT.java
+++ src/main/java/com/crm/qa/util/TestGIT.java
@@ -3,5 +3,6 @@ package com.crm.qa.util;
 public class TestGIT {
     public static void main(String[] args) {
         System.out.println("Testing git push");
+        System.out.println("Testing git pull from the git web");
     }
 }
