4d079ec1584efbdb890a67a0d9abee39c7142ea3
AutomationTestingD <sdk.wgl@gmaill.com>
2020-02-08 20:09:45 -0800

    new change

--- src/main/java/com/crm/qa/util/TestGIT.java
+++ src/main/java/com/crm/qa/util/TestGIT.java
@@ -8,6 +8,9 @@ public class TestGIT {
         System.out.println("commit file from idea");
         System.out.println("commit from Web to validate rebase");
         System.out.println("commit from Web to validate my script");
+        System.out.println("new changes");
 test
+
+
     }
 }
